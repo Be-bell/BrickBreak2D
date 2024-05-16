518020581e16b5dcc37ca424f6cce7fab6d7ca6b
양은수 <93758339+diddmstn@users.noreply.github.com>
2024-05-16 20:23:11 +0900

    Add Item Scripts
    아이템 관련 스크립트 추가+ 아이템 Prefab 추가

---
+++ Assets/02_Scripts/Item/BallDamageUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+public class BallDamageUp:Item
+{
+    private float ballDamage;
+    protected override void ItemEffect(GameObject ball)
+    {
+        Debug.Log("공의 데미지를 증가시킵니다.");
+        //Ball ball = ball.GetComponent<Ball>();
+        //ball.damage += ballDamage;
+        Destroy(this.gameObject);
+    }
+}
+
+
