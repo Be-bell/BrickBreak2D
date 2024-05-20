e8345f96dc7058e18db7f2c42f0f91df0b64272a
Be-bell <whdghk1346@khu.ac.kr>
2024-05-20 22:17:44 +0900

    >> Add DataManager
    DataManager, GameManager 추가

--- Assets/02_Scripts/01_Manager/DataManager.cs
+++ Assets/02_Scripts/01_Manager/DataManager.cs
@@ -1,3 +1,5 @@
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
 using UnityEngine;
 /// <summary>
 /// 단계 결정, 데이터 전달
@@ -7,17 +9,34 @@ public class DataManager : MonoBehaviour
     DataManager instance;
 
     public GameLevel level { get; private set; }
+    [SerializeField] private GameInformation gameInfo;
+    private List<LevelData> levelDataList;
+    public LevelData? currentLevelData { get; private set; }
 
     private void Awake()
     {
         if(instance == null)
         {
-            instance = new DataManager();
+            instance = this;
         }
 
         DontDestroyOnLoad(gameObject);
 
         level = GameLevel.EASY;
 
+    }
+
+    private void Start()
+    {
+        SetLevelData();   
+    }
+
+    private void SetLevelData()
+    {
+        levelDataList = Instantiate(gameInfo).dataList;
+        for (int i = 0; i < levelDataList.Count; i++)
+        {
+            currentLevelData = levelDataList[i].level == level ? levelDataList[i] : null;
+        }
     }
 }
--- Assets/02_Scripts/01_Manager/GameManager.cs
+++ Assets/02_Scripts/01_Manager/GameManager.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 /// <summary>
 /// 게임 로직 구성
@@ -6,16 +7,19 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
 
+    // 이벤트 관리
+    private event Action ballBreakEvent;
+    private event Action blockBreakEvent;
+
+    // 직렬화필드 관리
     [SerializeField] private ObjectPool objPool;
     [SerializeField] private string ballTag;
-
     // brick class 만들면 추후 기능 추가 예정
-    [SerializeField] private string brickTag;
-
-    // ball 개수
-    private int ballCount;
+    //[SerializeField] private string brickTag;
 
     public GameState nowState = GameState.GAME_READY;
+    private int blockCount;
+    private int ballCount;
 
     private void Awake()
     {
@@ -25,8 +29,41 @@ public class GameManager : MonoBehaviour
         }
 
         BallInstantiate();
+        ballBreakEvent += ballDestroy;
+        blockBreakEvent += blockDestroy;
         Time.timeScale = 1.0f;
     }
+
+    private void blockDestroy()
+    {
+        blockCount--;
+        if (blockCount == 0) GameClear();
+    }
+
+    // ball 삭제 후 따지기.
+    private void ballDestroy()
+    {
+        ballCount--;
+        if(ballCount == 0) GameOver();
+    }
+
+    private void GameClear()
+    {
+        // 여기서부터 UI 띄우는 거.
+        Debug.Log("게임 클리어!");
+        nowState = GameState.GAME_CLAER;
+        Time.timeScale = 0.0f;
+    }
+
+    private void GameOver()
+    {
+        // 여기서부터 UI창 띄우는 거.
+        Debug.Log("게임 오버");
+        nowState = GameState.GAME_OVER;
+        Time.timeScale = 0.0f;
+    }
+
+    /*****************************************************************************************************/
 
     // ball 생성 (Item 로직에서 사용가능.)
     public void BallInstantiate()
@@ -40,29 +77,41 @@ public class GameManager : MonoBehaviour
     }
 
     // ball count 빼기. 조금 더 좋은 생각 있는지 확인
-    public void SubstractBallCount()
+    //public void SubstractBallCount()
+    //{
+    //    ballCount--;
+    //}
+
+    //private void FixedUpdate()
+    //{
+    //    // ball 개수, brick 개수 세서 GameClaer나 GameOver 구현
+    //    GameUpdate();
+    //}
+
+    //// 블록 개수 0개 이하면 GameOver
+    //private void GameUpdate()
+    //{
+    //    if (ballCount <= 0)
+    //    {
+    //        Time.timeScale = 0.0f;
+    //        Debug.Log("게임 오버");
+    //        nowState = GameState.GAME_OVER;
+    //    }
+
+    //    Debug.Log(nowState);
+
+    //}
+
+    /*****************************************************************************************************/
+
+    public void NotifyBallBreakEvent()
     {
-        ballCount--;
+        ballBreakEvent?.Invoke();
     }
 
-    private void FixedUpdate()
+    public void NotifyBlockBreakEvent()
     {
-        // ball 개수, brick 개수 세서 GameClaer나 GameOver 구현
-        GameUpdate();
+        blockBreakEvent?.Invoke();
     }
 
-    // 블록 개수 0개 이하면 GameOver
-    private void GameUpdate()
-    {
-        if (ballCount <= 0)
-        {
-            Time.timeScale = 0.0f;
-            Debug.Log("게임 오버");
-            nowState = GameState.GAME_OVER;
-        }
-
-        Debug.Log(nowState);
-        
-    }
-    
 }
