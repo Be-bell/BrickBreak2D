c4fad2c2ac5444d029f5b95db0b59049b642de35
Be-bell <whdghk1346@khu.ac.kr>
2024-05-20 22:17:04 +0900

    >> Change Wall
    Wall 바꿈

--- Assets/02_Scripts/02_Objects/BackWall.cs
+++ Assets/02_Scripts/02_Objects/BackWall.cs
@@ -1,21 +1,37 @@
 using UnityEngine;
 
-public class BackWall : MonoBehaviour
+public class BackWall : MonoBehaviour, Icollidable
 {
-    [SerializeField] private LayerMask ballLayer;
+    [SerializeField] private GameManager gameManager;
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void Start()
     {
-        // 레이어로 일치하는지 처리
-        if (IsLayerMatched(ballLayer.value, collision.gameObject.layer))
+        if(gameManager == null)
         {
-            GameManager.instance.SubstractBallCount();
-            GameObject.Destroy(collision.gameObject);
+            gameManager = GameManager.instance;
         }
     }
+    //[SerializeField] private LayerMask ballLayer;
 
-    private bool IsLayerMatched(int layerMask, int objectLayer)
+    //private void OnCollisionEnter2D(Collision2D collision)
+    //{
+    //    // 레이어로 일치하는지 처리
+    //    if (IsLayerMatched(ballLayer.value, collision.gameObject.layer))
+    //    {
+    //        GameManager.instance.SubstractBallCount();
+    //        GameObject.Destroy(collision.gameObject);
+    //        //collision.gameObject.SetActive(false);
+    //    }
+    //}
+
+    //private bool IsLayerMatched(int layerMask, int objectLayer)
+    //{
+    //    return layerMask == (layerMask | (1 << objectLayer));
+    //}
+
+    public void OnCollide(GameObject ball)
     {
-        return layerMask == (layerMask | (1 << objectLayer));
+        ball.SetActive(false);
+        gameManager.NotifyBallBreakEvent();
     }
 }
