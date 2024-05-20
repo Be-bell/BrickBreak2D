dabaabc6382c5c91af450697fef3bfac9e35d2d3
birdham <139436177+birdham@users.noreply.github.com>
2024-05-20 10:14:27 +0900

    2024_05_20

--- Assets/02_Scripts/Brick/BrickGenerator.cs
+++ Assets/02_Scripts/Brick/BrickGenerator.cs
@@ -2,15 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BrickGenorator : MonoBehaviour, IBrickPattern
+public class BrickGenorator : MonoBehaviour, IBrickPattern //IBrickGradient
 {
     public Vector2Int size;
     public Vector2 offset;
     public GameObject brickPreFab;
     public Gradient gradient;
-
+    public Gradient customGradient; // 사용자 정의 그라데이션
     // Start is called before the first frame update
-    public void Awake()
+    public void  Awake()
     {
 
         for (int i = 0; i < size.x; i++)
@@ -20,9 +20,11 @@ public class BrickGenorator : MonoBehaviour, IBrickPattern
                 // 체크 모양을 생성하는 조건 추가
                 if ((i + j) % 2 == 0)
                 {
+                    
+                    // 그라데이션 변수에 새로운 그라데이션 설정
                     GameObject newBrick = Instantiate(brickPreFab, transform);
                     newBrick.transform.position = transform.position + new Vector3((float)((size.x - 1) * 0.5f - i) * offset.x, j * offset.y, 0);
-                    newBrick.GetComponent<SpriteRenderer>().color = gradient.Evaluate((float)j / (size.y - 1));
+                    newBrick.GetComponent<SpriteRenderer>().color = customGradient.Evaluate((float)j / (size.y - 1));
                 }
             }
         }
--- Assets/02_Scripts/Brick/IBrickPattern.cs
+++ Assets/02_Scripts/Brick/IBrickPattern.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public interface IBrickPattern : MonoBehaviour
+public interface IBrickPattern
 {
     // Start is called before the first frame update
     void Start()
