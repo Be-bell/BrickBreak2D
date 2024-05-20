f8c63ee78f26d7c0303fab749627b927c82fd615
unm7925 <unm7925@gmail.com>
2024-05-20 10:36:54 +0900

    Fix Iocllidable.cs

--- Assets/02_Scripts/Icollidable.cs
+++ Assets/02_Scripts/Icollidable.cs
@@ -1,20 +1,7 @@
-<<<<<<< HEAD
 using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface Icollidable
-{
-
-    void OnCollide(GameObject Ball);
-
-}
-=======
-﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 public interface Icollidable
 {
     public void OnCollide(GameObject Ball);
 }
->>>>>>> f220808c11c93a5849f71889198b2106f027e3f4
