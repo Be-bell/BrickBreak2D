af56e2de2c04d17ba22d08689dff62ffb723c514
birdham <139436177+birdham@users.noreply.github.com>
2024-05-21 12:35:57 +0900

    new

--- Assets/02_Scripts/Brick/Brick.cs
+++ Assets/02_Scripts/Brick/Brick.cs
@@ -4,21 +4,49 @@ using UnityEngine;
 
 public class Brick : MonoBehaviour
 {
-    int brickhp;
-    
-    // Start is called before the first frame update
-    void Start()
+    public SpriteRenderer spriteRenderer { get; private set; }
+    public Sprite[] states;
+    public int health {  get; private set; }
+
+    public bool unbreakable;
+
+    private void Awake()
     {
-        
+        this.spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        if (!this.unbreakable)
+        {
+            this.health = this.states.Length;
+            this.spriteRenderer.sprite = this.states[this.health - 1];
+        }
     }
-    void OnCollisionEnter(Collision collision)
+
+    private void Hit()
     {
-        brickhp -= brickhp;
+        if (this.unbreakable)
+        {
+            return;
+        }
+        this.health--;
+
+        if (this.health <= 0)
+        {
+            this.gameObject.SetActive(false);
+        }
+        else
+        {
+            this.spriteRenderer.sprite = this.states[this.health - 1];
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "Ball")
+        {
+            Hit();
+        }
     }
 }
+
