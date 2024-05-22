e562dec689352b1b3e6333e5a999a372237b0baa
양은수 <93758339+diddmstn@users.noreply.github.com>
2024-05-22 15:02:21 +0900

    >> Fix BallCountUp Script
    오브젝트 풀 적용

--- Assets/02_Scripts/Item/BallCountUp.cs
+++ Assets/02_Scripts/Item/BallCountUp.cs
@@ -3,12 +3,11 @@
 public class BallCountUp : Item
 {
     [SerializeField]private int ballCount = 2; //일단 2개만 복제
-    [SerializeField] private GameObject ball;
     protected override void ItemEffect(GameObject obj)
     {
         for(int i=0; i<ballCount;i++)
         {
-            Instantiate(ball);
+            GameManager.instance.BallInstantiate();
         }
         ItemDestroy();
     }
